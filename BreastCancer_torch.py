ea11109638fd0399a00eacc76016dbc34927c46a
Yamaç Topal <96369324+YamacTopal@users.noreply.github.com>
2024-05-17 10:23:47 +0300

    Update BreastCancer_torch.py

--- BreastCancer_torch.py
+++ BreastCancer_torch.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 12 12:44:14 2024
-
-@author: Yamaç
-"""
-
 from ucimlrepo import fetch_ucirepo 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
